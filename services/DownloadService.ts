bc9dbf8bff82c8956b7dbf375be04c8992388c85
Imre Gábor doktorandusz <i.gabesz@aut.bme.hu>
2016-04-19 13:54:17 +0200

    progress(lab10): classwork

--- services/DownloadService.ts
+++ services/DownloadService.ts
@@ -1,8 +1,19 @@
 class DownloadService {
+    
+    static $inject = ['$http'];
+    constructor(private $http: ng.IHttpService) {}
 
 	downloadJoke() {
-		// Get jokes from here:
-		// http://api.icndb.com/jokes/random?exclude=[explicit]
-		// Then format it to IChuckJoke and return it
+        return this.$http.get<any>('http://api.icndb.com/jokes/random')
+        .then((result) => {
+            if (result.data.type !== 'success')
+                throw new Error('Cannot download');
+            let data = result.data.value;
+            return <IChuckJoke>{
+                id: data.id,
+                joke: data.joke,
+                isNerdy: data.categories.indexOf('nerdy') !== -1,
+            };
+        });
 	}
 }
